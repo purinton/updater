dfe8dca5a1d7ebb08ed9beea30f3dc9a27efe23c
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2025-06-19 19:02:46 -0400

    Update update.mjs

--- update.mjs
+++ update.mjs
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import 'dotenv/config';
 import { registerHandlers, registerSignals, log as logger, path as commonPath } from '@purinton/common';
+import { sshExec } from '@purinton/ssh-client';
 import runUpdater from './src/runUpdater.mjs';
 
 // CLI entrypoint
@@ -8,7 +9,7 @@ if (process.env.NODE_ENV !== 'test') {
     registerHandlers({ log: logger });
     registerSignals({ log: logger });
     const serversFile = commonPath(import.meta, 'servers.json');
-    runUpdater({ serversFile }).catch(err => {
+    runUpdater({ serversFile, sshExec }).catch(err => {
         logger.error('Fatal error in updater', err);
         process.exit(1);
     });
