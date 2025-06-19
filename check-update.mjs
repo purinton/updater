7e21e876baaa16f5ac6af2b785fcfa12446c12e2
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2025-06-19 19:10:52 -0400

    Simplify Entrypoints

--- check-update.mjs
+++ check-update.mjs
@@ -1,34 +1,23 @@
 #!/usr/bin/env node
 import 'dotenv/config';
-import {
-    log as logger,
-    registerHandlers,
-    registerSignals,
-    path as commonPath
-} from '@purinton/common';
-import cron from 'node-cron';
-import { runCheckForAllServers } from './src/runCheckForAllServers.mjs';
 
-export async function main(options = {}) {
-    await runCheckForAllServers(options);
-}
+import { log, path, registerHandlers, registerSignals } from '@purinton/common';
+import { runCheckForAllServers } from './src/runCheckForAllServers.mjs';
 
 // CLI entrypoint
 if (process.env.NODE_ENV !== 'test') {
-    registerHandlers({ log: logger });
-    registerSignals({ log: logger });
-    const serversFile = commonPath(import.meta, 'servers.json');
-    main({ serversFile }).catch(err => {
-        logger.error('Fatal error in main', err);
+    registerHandlers({ log });
+    registerSignals({ log });
+    const serversFile = path(import.meta, 'servers.json');
+    runCheckForAllServers({ serversFile }).catch(err => {
+        log.error('Fatal error in main', err);
         process.exit(1);
     });
-    // Schedule to run at the top of each hour
     cron.schedule('0 * * * *', () => {
-        main().catch(err => {
-            logger.error('Fatal error in scheduled main', err);
+        runCheckForAllServers({ serversFile }).catch(err => {
+            log.error('Fatal error in scheduled main', err);
         });
     });
-    // Keep process alive
     setInterval(() => { }, 1 << 30);
 }
 
--- update.mjs
+++ update.mjs
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 import 'dotenv/config';
-import { registerHandlers, registerSignals, log as logger, path as commonPath } from '@purinton/common';
+import { log, path, registerHandlers, registerSignals } from '@purinton/common';
 import { sshExec } from '@purinton/ssh-client';
 import runUpdater from './src/runUpdater.mjs';
 
 // CLI entrypoint
 if (process.env.NODE_ENV !== 'test') {
-    registerHandlers({ log: logger });
-    registerSignals({ log: logger });
-    const serversFile = commonPath(import.meta, 'servers.json');
+    registerHandlers({ log });
+    registerSignals({ log });
+    const serversFile = path(import.meta, 'servers.json');
     runUpdater({ serversFile, sshExec }).catch(err => {
-        logger.error('Fatal error in updater', err);
+        log.error('Fatal error in updater', err);
         process.exit(1);
     });
 }
